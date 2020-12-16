a3d49f8f0f8948b8b4269c5b52dd77a4d304b5b3
Dayyan Smith <dayyan.smith@gmail.com>
2020-12-16 10:44:26 +0100

    Update output dir for sagemaker

--- domainbed/scripts/train.py
+++ domainbed/scripts/train.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     parser.add_argument('--checkpoint_freq', type=int, default=None,
         help='Checkpoint every N steps. Default is dataset-dependent.')
     parser.add_argument('--test_envs', type=int, nargs='+', default=[0])
-    parser.add_argument('--output_dir', type=str, default=os.environ["SM_MODEL_DIR"] if "SM_MODEL_DIR" in os.environ else "train_output")
+    parser.add_argument('--output_dir', type=str, default='/opt/ml/model' if "AWS_REGION" in os.environ else "train_output")
     parser.add_argument('--holdout_fraction', type=float, default=0.2)
     parser.add_argument('--skip_model_save', action='store_true')
     parser.add_argument('--skip_acc_calc', action='store_true')
